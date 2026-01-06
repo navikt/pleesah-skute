5726070bf6d10879fdbea977cb6c3f44f8ab6107
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2026-01-06 10:19:01 +0100

    ✨ Sjekker readinessprobe om ankeret er hevet
    Co-authored-by: Kyrre Havik <kyrre.havik@nav.no>
    Co-authored-by: Tania Holst <tania.holst@nav.no>
    Co-authored-by: Emil Elton Nilsen <emil.elton.nilsen@nav.no>

--- src/main/kotlin/org/example/App.kt
+++ src/main/kotlin/org/example/App.kt
@@ -1,9 +1,13 @@
 package org.example
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.runBlocking
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 fun main() {
     embeddedServer(Netty, 8080) {
@@ -12,8 +16,25 @@ fun main() {
                 call.respondText("OK")
             }
             get("/isReady") {
-                call.respondText("OK")
+                sjekkReadinessProbe(this)
             }
         }
     }.start(wait = true)
 }
+
+val log: Logger = LoggerFactory.getLogger("main")
+
+fun sjekkReadinessProbe(context: RoutingContext) {
+    runBlocking {
+        val anker = System.getenv("HEV_ANKER")
+        if (anker == "true") {
+            log.info("Ankeret er hevet")
+            context.call.respondText("Ankeret er hevet")
+        } else {
+            log.info("HEV_ANKER er ikke satt til true")
+            context.call.respondText(status = HttpStatusCode.TooEarly, text = "HEV_ANKER er ikke satt til true")
+        }
+    }
+}
+
+
