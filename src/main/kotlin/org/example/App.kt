4e17cb162af8bc422c3d25b3df986f2cf1b63c68
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2026-01-07 15:27:10 +0100

    ♻️ sjekker for secret uten kobling til livnessprobe
    Co-authored-by: Kyrre Havik <kyrre.havik@nav.no>
    Co-authored-by: Emil Elton Nilsen <emil.elton.nilsen@nav.no>

--- src/main/kotlin/org/example/App.kt
+++ src/main/kotlin/org/example/App.kt
@@ -29,11 +29,21 @@ fun sjekkReadinessProbe(context: RoutingContext) {
         val loss = System.getenv("HAR_KASTET_LOSS")
         if (loss == "true") {
             log.info("Hurra! Du har kastet loss og er klar til å plyndre!")
+            sjekkSecret()
             context.call.respond(HttpStatusCode.OK)
         } else {
             log.info("Skuta er fortsatt fortøyd til havna og er ikke klar til å seile til sjøs. Du må kaste loss slik at skuta er klar for å seile. Kubernetes bruker en readiness probe for å sjekke om poden er klar for å ta imot trafikk. I denne oppgaven har vi gjort det slik at skuta krever at en miljøvariabel HAR_KASTET_LOSS er satt til 'true'. I den virkelige verden vil det være opp til hver enkelt å bestemme hvordan gi beskjed til kubernetes om at poden er klar.")
             context.call.respond(HttpStatusCode.NotImplemented)
         }
+    }
+}
+
+fun sjekkSecret() {
+    val hemmelighet = System.getenv("HAR_SATT_KURS")
+    if (hemmelighet == "59.9124° N, 10.7962° E") {
+        log.info("Kursen er satt og du er endelig på vei til din destinasjon!")
+    } else {
+        log.info("Hurra! Du har kastet loss og er snart klar til å plyndre. Men hvor skal vi, egentlig? Koordinatene finner du i en hemmelighet! I K8s kan hemmeligheter lagres i ressurstypen secrets. Disse kan inneholde forskjellig typer data, men i vår instans finnes det kun én nøkkel skuta trenger for å sette kurs mot riktig desinasjon")
     }
 }
 
