ab3fb712be51ff4db018b4a1b68c6a97d4a01893
Tania Holst <holst.tania@gmail.com>
2026-01-07 15:12:40 +0100

    :green_heart: Fjerna livenessprobe og endra tekst

--- src/main/kotlin/org/example/App.kt
+++ src/main/kotlin/org/example/App.kt
@@ -13,7 +13,7 @@ fun main() {
     embeddedServer(Netty, 8080) {
         routing {
             get("/isAlive") {
-                sjekkAlivenessProbe(this)
+                call.respond(HttpStatusCode.OK)
             }
             get("/isReady") {
                 sjekkReadinessProbe(this)
@@ -28,41 +28,13 @@ fun sjekkReadinessProbe(context: RoutingContext) {
     runBlocking {
         val loss = System.getenv("HAR_KASTET_LOSS")
         if (loss == "true") {
-            log.info("Hurra! Du har kastet loss og skuta er klar for å plyndre")
-            context.call.respondText(
-                status = HttpStatusCode.OK,
-                text = "Hurra! Du har kastet loss og skuta er klar for å plyndre"
-            )
+            log.info("Hurra! Du har kastet loss og er klar til å plyndre!")
+            context.call.respond(HttpStatusCode.OK)
         } else {
-            log.info(
-                "Skuta er fortsatt fortøyd til havna og er ikke klar til å seile til sjøs. Du må kaste loss slik at skuta er klar for å seile. Kubernetes bruker en readiness probe for å sjekke om poden er klar for å ta imot trafikk. I denne oppgaven har vi gjort det slik at skuta krever at en miljøvariabel HAR_KASTET_LOSS er satt til 'true'. I den virkelige verden vil det være opp til hver enkelt å bestemme hvordan gi beskjed til kubernetes om at poden er klar."
-            )
-            context.call.respondText(
-                status = HttpStatusCode.NotImplemented,
-                text = "Skuta er fortsatt fortøyd til havna og er ikke klar til å seile til sjøs. Du må kaste loss slik at skuta er klar for å seile. Kubernetes bruker en readiness probe for å sjekke om poden er klar for å ta imot trafikk. I denne oppgaven har vi gjort det slik at skuta krever at en miljøvariabel HAR_KASTET_LOSS er satt til 'true'. I den virkelige verden vil det være opp til hver enkelt å bestemme hvordan gi beskjed til kubernetes om at poden er klar."
-            )
+            log.info("Skuta er fortsatt fortøyd til havna og er ikke klar til å seile til sjøs. Du må kaste loss slik at skuta er klar for å seile. Kubernetes bruker en readiness probe for å sjekke om poden er klar for å ta imot trafikk. I denne oppgaven har vi gjort det slik at skuta krever at en miljøvariabel HAR_KASTET_LOSS er satt til 'true'. I den virkelige verden vil det være opp til hver enkelt å bestemme hvordan gi beskjed til kubernetes om at poden er klar.")
+            context.call.respond(HttpStatusCode.NotImplemented)
         }
     }
 }
 
-fun sjekkAlivenessProbe(context: RoutingContext) {
-    runBlocking {
-        if (System.getenv("HAR_KASTET_LOSS") != "true") {
-            context.call.respondText("OK")
-        }
-
-        val seil = System.getenv("HAR_SATT_SEIL")
-        if (seil == "true") {
-            log.info("Seilet er satt")
-            context.call.respondText(status = HttpStatusCode.OK, text = "Seilet er satt")
-        } else {
-            log.info("HAR_SATT_SEIL secreten er ikke satt til true")
-            context.call.respondText(
-                status = HttpStatusCode(510, description = "Not extended"),
-                text = "HAR_SATT_SEIL secreten er ikke satt til true"
-            )
-
-        }
-    }
-}
 
